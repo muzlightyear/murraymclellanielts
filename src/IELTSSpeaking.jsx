ef1da4c665512689cb2d6ddf83215ccf8c769844
Murray Mclellan <102920557+muzlightyear@users.noreply.github.com>
2026-07-06 23:04:07 +0700

    Added supabase integration and env integration.

--- src/IELTSSpeaking.jsx
+++ src/IELTSSpeaking.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { supabase } from './supabaseClient';
 
 function IELTSSpeaking() {
+
+    const { data } = supabase
+    .storage
+    .from('murraymclellanielts-resources/speaking-tests')
+    .getPublicUrl('ielts-speaking-test-speaking-test-1.pdf')
+    const pdfUrl = data.publicUrl;
+
     return (
         <div className="ieltsspeaking-container">
-            <a>THIS IS THE IELTS SPEAKING PAGE.</a>
+            <a>IELTS Speaking Sample Test 1</a>
+            <iframe src={pdfUrl} width="100%" height="600px" />
+            <a>IELTS Speaking Sample Test 2</a>
         </div>
     )
 }
-
 export default IELTSSpeaking
