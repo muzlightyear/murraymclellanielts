a57e2ebf4d213b04e6b8ab52821d151f72015121
Murray's Laptop <murraymclellan@hotmail.co.uk>
2026-07-08 10:22:48 +0700

    Trying to re-commit all of the work from yesterday.

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import './App.css'
+import cert1 from './assets/certificatePic1.png';
+import cert2 from './assets/certificatePic2.png';
+import vnFlag from './assets/vietnamese-flag.PNG';
 function About() {
     return (
         <div className="about-container">
-            <a>THIS IS THE ABOUT PAGE.</a>
+            <div className="about-paragraph">
+            <h1>About Murray</h1>
+            <p>My name is Murray. I'm from Stroud, a relatively small city in England(UK) & I have been teaching English since December of 2015. I can speak English (Native) & Vietnamese (C1 - Business Proficiency). I have a passion for languages, video game development, web development, music, and horror movies.</p>
+            <p>ABOUT MY TEACHING<br></br>
+            I've been teaching since 2015, both online and offline. I have worked for a multitude of institutions as listed below:</p>
+            <li>🏫International schools</li>
+            <li>🏫English centers</li>
+            <li>🏫Private schools</li>
+            <li>🏫Public schools</li>
+            <li>🏫Online English centers</li>
+            <a>✔️I am a British Council and IDP certified IELTS tutor, and I have formal 1-1 training from an ex-IELTS examiner.</a>
+            </div>
+
+            <div className="ielts-expert-container">
+            <h1 className="ielts-expert-title">IELTS Expert</h1>
+            <img className="certificateImg" src={cert1}></img>
+            <img className="certificateImg" src={cert2}></img>
+            </div>
+
+             <h1>Bilingual</h1>
+            <div className="bilingual-description-container">
+            <img className="vietnam-flag-png" src={vnFlag}></img>
+            <p className="bilingual-description">I have been living in Vietnam since 2018. Due to my assimilation to the culture, my Vietnamese is at a near-native level (C1), so I may be a good fit for Vietnamese learners.</p>
+            </div>
         </div>
     )
 }
--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import shanti from './assets/shanti.jpg'
+import './Feedback.css'
 
 function Feedback() {
     return (
         <div className="feedback-container">
-            <a>THIS IS THE FEEDBACK PAGE.</a>
+            <a>Structure: Img + Description.</a>
+        <img src={shanti}></img>
+        <p>“You explained everything in detail and it was easy to understand, and because I am used to listening to Indonesian teachers and this is my first time having a native speaker’s tutor, I sometimes found it hard. But after all, you are my hero, and I got my goal score in just a few days.” Shanti – Aim: IELTS Speaking Band 6.5</p>
         </div>
     )
 }
