32fb3cc64790c5eb8976b73eebc7f438090d9e1e
Murray's Laptop <murraymclellan@hotmail.co.uk>
2026-07-08 16:10:55 +0700

    Started adding student feeback and styling it.

--- src/Feedback.jsx
+++ src/Feedback.jsx
@@ -1,13 +1,78 @@
 import React from 'react'
 import shanti from './assets/shanti.jpg'
+import hoanganh from './assets/hoanganh.PNG'
+import kenneth from './assets/kenneth.jpg'
+import nasr from './assets/nasr.png'
+import halan from './assets/halan.png'
+
 import './Feedback.css'
 
 function Feedback() {
     return (
         <div className="feedback-container">
-            <a>Structure: Img + Description.</a>
-        <img src={shanti}></img>
-        <p>“You explained everything in detail and it was easy to understand, and because I am used to listening to Indonesian teachers and this is my first time having a native speaker’s tutor, I sometimes found it hard. But after all, you are my hero, and I got my goal score in just a few days.” Shanti – Aim: IELTS Speaking Band 6.5</p>
+
+            <div className="feedback-block">
+                <a className="header-text">9.0 Overall.</a>
+                <div className="written-feedback">
+                <p>“Hey, Murray, here is my result. i finally made it.  I still wanna give you a big shoutout.thanks for being an important part of my ielts journey.” – Yang</p>
+                </div>
+            </div>
+
+            <div className="feedback-block">
+                <a className="header-text">7.5 Overall.</a>
+                <div className="written-feedback">
+                <p>“Hey Murray,
+
+I hope you’re doing well and not too busy with work on holidays!
+
+I’ve just received my results! I attached the document but I can’t find the other one that shows more specific grading. Do you know where I can see it, by any chance?
+
+I’ve clearly messed up with my writing, so I’m very happy with this band, didn’t expect reading to be so high, though; I was struggling. And I was sure that I did better in listening, but I can’t complain lol. The speaking part was a whole other story. I literally blacked out and now I don’t remember a single question from the examiner except “Are you a student or do you work?”.
+
+Wish I could yapp about it over our lesson because none of my friends understand the stress I’ve been through, but I’m very very grateful for your help!! Thank you!
+
+Happy New Year’s Eve!
+
+All the best,
+
+Yasmina” Yasmina</p>
+                </div>
+            </div>
+
+            <div className="feedback-block">
+                <a className="header-text">7.0 Overall.</a>
+                <div className="written-feedback">
+                <p>“Thank you very much for your help in preparing for the exam. Your guidance allowed me to achieve the results I hoped for in a remarkably short time. Also, your clear explanations made a huge difference and helped me a lot.” Mikhail</p>
+                </div>
+            </div>
+
+            <div className="feedback-block">
+                <a className="header-text">7.5 Overall.</a>
+                <div className="written-feedback">
+                <p>“It was really good and easy to follow and you gave me a lot of practice to make me get used to answering more properly as soon as I could, which I very much appreciated.” Nguyễn Hạ Lan</p>
+                </div>
+            </div>
+
+            <div className="feedback-block">
+                <a className="header-text">8.0 Overall.</a>
+                <div className="written-feedback">
+                <p>“Nasr got scholarships about $5,000 for his foundation course according to his score of IELTS. Thank you so much for your support for Nasr.” Nasr’s Mum</p>
+                </div>
+            </div>
+
+            <div className="feedback-block">
+                <a className="header-text">7.0 Overall.</a>
+                <div className="written-feedback">
+                <p>“I feel great, the school accepted me, and I’m going to do the visa.” Hoàng Anh</p>
+                </div>
+            </div>
+
+            <div className="feedback-block">
+                <a className="header-text">6.5 Overall.</a>
+                <div className="written-feedback">
+                <p>“You explained everything in detail and it was easy to understand, and because I am used to listening to Indonesian teachers and this is my first time having a native speaker’s tutor, I sometimes found it hard. But after all, you are my hero, and I got my goal score in just a few days.” Shanti</p>
+                </div>
+            </div>
         </div>
     )
 }
